1f6b6026cff778e302c5c7ea89e1086e1b980830
unknown <19251197@bsi.ac.id>
2026-04-17 15:04:06 +0700

    latihan1

--- latihan1.php
+++ latihan1.php
@@ -3,9 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>latihan1</title>
 </head>
 <body>
-    
+     <h1>latihan1</h1>
+     <table border="1">
+         <tr>
+             <th>Nama</th>
+             <th>Usia</th>
+             <th>No Hp</th>
+         </tr>
+         <tr>
+             <td>John Doe</td>
+             <td>30</td>
+             <td>08123456789</td>
+         </tr>
+         <tr>
+             <td>Sekar</td>
+             <td>19</td>
+             <td>08987654321</td>
+         </tr>
+     </table>
 </body>
 </html>
