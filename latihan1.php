583b86b8118dc672f2e7ee57f3417f3a3df8700c
unknown <19251197@bsi.ac.id>
2026-04-17 15:15:16 +0700

    perubahan

--- latihan1.php
+++ latihan1.php
@@ -4,25 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>latihan1</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-     <h1>latihan1</h1>
-     <table border="1">
-         <tr>
-             <th>Nama</th>
-             <th>Usia</th>
-             <th>No Hp</th>
-         </tr>
-         <tr>
-             <td>John Doe</td>
-             <td>30</td>
-             <td>08123456789</td>
-         </tr>
-         <tr>
-             <td>Sekar</td>
-             <td>19</td>
-             <td>08987654321</td>
-         </tr>
+<body class="p-8 bg-gray-50">
+     <h1 class="text-2xl font-bold mb-4">latihan1</h1>
+     <table class="table-auto w-full">
+         <thead class="bg-gray-100">
+             <tr class="border-b border-gray-300">
+                 <th class="px-4 py-2 text-left">Nama</th>
+                 <th class="px-4 py-2 text-left">Usia</th>
+                 <th class="px-4 py-2 text-left">No Hp</th>
+             </tr>
+         </thead>
+         <tbody>
+             <tr class="border-b border-gray-300">
+                 <td class="px-4 py-2">John Doe</td>
+                 <td class="px-4 py-2">30</td>
+                 <td class="px-4 py-2">08123456789</td>
+             </tr>
+             <tr class="border-b border-gray-300">
+                 <td class="px-4 py-2">Sekar</td>
+                 <td class="px-4 py-2">19</td>
+                 <td class="px-4 py-2">08987654321</td>
+             </tr>
+         </tbody>
      </table>
 </body>
 </html>
